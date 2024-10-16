65e0f6c466e63a04236f72a3d519a85f26fcef02
yraj2001 <yaswamthrajvarikunta@gmail.com>
2024-10-15 23:39:08 -0500

    Clean up the game Cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,11 +13,11 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={game.image.small_url} height="400px" />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList platforms={game.platforms} />
           <NoOfReviews no={game.number_of_user_reviews} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
